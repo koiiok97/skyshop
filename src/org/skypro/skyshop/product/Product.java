f1766b58c0ebf9264ac5a4a2f6144f2db838d3fd
capybara97 <bezdomnyi1997@gmail.com>
2025-03-29 00:24:54 +0500

    hw7

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -2,6 +2,8 @@ package org.skypro.skyshop.product;
 
 import org.skypro.skyshop.search.Searchable;
 
+import java.util.Objects;
+
 public abstract class Product implements Searchable {
     protected final String name;
 
@@ -21,12 +23,29 @@ public abstract class Product implements Searchable {
     }
 
     @Override
-    public String getSearchTerm(){
+    public String getSearchTerm() {
         return getName();
     }
 
     @Override
-    public String getContentType(){
+    public String getContentType() {
         return "PRODUCT";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        return Objects.equals(name, ((Product) o).name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        int comp = Integer.compare(((Product) o).getSearchTerm().length(), name.length());
+        return comp != 0 ? comp : name.compareTo(((Product) o).getSearchTerm());
     }
 }
--- src/org/skypro/skyshop/search/Searchable.java
+++ src/org/skypro/skyshop/search/Searchable.java
@@ -1,12 +1,16 @@
 package org.skypro.skyshop.search;
 
-public interface Searchable {
+public interface Searchable extends Comparable {
 
     String getSearchTerm();
 
     String getContentType();
 
-    default String getStringRepresentation(){
+    default String getStringRepresentation() {
         return "имя " + getSearchTerm() + " - объекта — тип " + getContentType() + " - объекта";
     }
+
+
+    @Override
+    int compareTo(Object o);
 }
